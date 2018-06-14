97e829100480a03af4ae3fcfcdfaea255b93f70e
040305 <040305@USHOM00NB040305>
2018-06-14 10:58:48 -0500

    deleteing the files

--- PrivateLabelLite/Assets/js/filter/boolToYesNo.js
+++
@@ -1,8 +0,0 @@
-﻿(function () {
-    app.filter('boolToYesNo', function () {
-        return function (input) {
-            input = input || '';
-            return input == true || input.toString().toLowerCase() == 'true' ? 'Yes' : 'No';
-        }
-    })
-})();
--- PrivateLabelLite/Raw/Assets/js/app.js
+++
@@ -1,6 +0,0 @@
-﻿(function () {
-    'use strict'; 
-
-    window.app = angular.module('app', ['lazyLoader','customPagination','typeahead']);
-})();
-
