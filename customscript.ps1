ecc5e5cc26d5882893330af40e0174ebb730d3a2
040305 <040305@USHOM00NB040305>
2018-06-18 12:01:42 -0500

    123

--- customscript.ps1
+++ customscript.ps1
@@ -1,3 +1,9 @@
 Write-Host "hellorajesh**************";
-Write-Host $branch
+# Make outputs from resource group deployment available to subsequent tasks
+Write-Host $outputs.branch.Value
+
+param ([string]$resourceGroup)
+$outputs = (Get-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroup | Select -Last 1).Outputs
+$outputs.Keys | % { Write-Host ("##vso[task.setvariable variable="+$_+";]"+$outputs.branch.Value) }
+
 
