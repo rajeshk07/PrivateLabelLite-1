d02722018d8c4a07782289f847d53548df8a9401
040305 <040305@USHOM00NB040305>
2018-06-18 16:43:58 -0500

    43

--- deployment/postdeployactions/customscript.ps1
+++ deployment/postdeployactions/customscript.ps1
@@ -1,5 +1,5 @@
 Write-Host "hellorajesh**************";
-Requires -Module AzureRM.Resources
+#Requires -Module AzureRM.Resources
 # Make outputs from resource group deployment available to subsequent tasks
-Write-Host $outputs.branch.Value;
+Write-Host $outputs.branchCode.Value;
 
