6f7f5c596c722749eaf82b1df014e4282ee2f388
040305 <040305@USHOM00NB040305>
2018-06-19 11:01:11 -0500

    32

--- deployment/postdeployactions/customscript.ps1
+++ deployment/postdeployactions/customscript.ps1
@@ -2,12 +2,7 @@ Write-Output "hellorajesh**************";
 
 gci env:* | sort-object name;
 
-sitename = Get-ChildItem Env:WEBSITE_RESOURCE_GROUP
-
-Get-AzureRmResourceGroup -Name "EngineerBlog"
-
-
-Get-ChildItem Env:SQLCONNSTR_DefaultConnection
+Get-ChildItem Env:SQLCONNSTR_DefaultConnection;
 
 Write-Output $SQLCONNSTR_DefaultConnection;
 # Make outputs from resource group deployment available to subsequent tasks
