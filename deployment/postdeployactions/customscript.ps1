2a8d5dc662a6273041eea263d5bbc8b2be74d2ca
040305 <040305@USHOM00NB040305>
2018-06-19 07:54:04 -0500

    34

--- deployment/postdeployactions/customscript.ps1
+++ deployment/postdeployactions/customscript.ps1
@@ -1,4 +1,7 @@
 Write-Output "hellorajesh**************";
+
+gci env:* | sort-object name;
+Write-Output $SQLCONNSTR_DefaultConnection;
 # Make outputs from resource group deployment available to subsequent tasks
 Write-Output $outputs;
 Write-Output $outputs.branchCode.Value;
