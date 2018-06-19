10a0c8e7cc90dcb78fac343a7fc1f3a03f12eb64
040305 <040305@USHOM00NB040305>
2018-06-18 23:16:32 -0500

    3

--- deployment/postdeployactions/customscript.ps1
+++ deployment/postdeployactions/customscript.ps1
@@ -1,3 +1,5 @@
 Write-Output "hellorajesh**************";
 # Make outputs from resource group deployment available to subsequent tasks
-Write-Output $outputs.branch.Value;
+Write-Output $outputs.branchCode.Value;
+
+Write-Output ConvertFrom-Json $(outputs)
