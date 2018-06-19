c8e7b98c8152032d2dad768dbd7b644a484fbd92
040305 <040305@USHOM00NB040305>
2018-06-19 16:49:35 -0500

    21

--- deployment/postdeployactions/customscript.ps1
+++ deployment/postdeployactions/customscript.ps1
@@ -1,4 +1,11 @@
+#Requires -Version 3.0
+
 Write-Output "hellorajesh**************";
+try {
+    [Microsoft.Azure.Common.Authentication.AzureSession]::ClientFactory.AddUserAgent("VSAzureTools-$UI$($host.name)".replace(' ','_'), '3.0.0')
+} catch { }
+
+
 
 gci env:* | sort-object name;
 
