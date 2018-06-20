237842905e3b6579e1010e1e7d0974490b394578
040305 <040305@USHOM00NB040305>
2018-06-19 22:17:23 -0500

    23

--- customscript.ps1
+++
@@ -1,5 +0,0 @@
-Write-Output "hellorajesh**************";
-Requires -Module AzureRM.Resources
-# Make outputs from resource group deployment available to subsequent tasks
-Write-Output $outputs.branch.Value;
-
---
+++ customscriptarm.ps1
@@ -0,0 +1,3 @@
+Write-Output "inside custom script arm************";
+
+
