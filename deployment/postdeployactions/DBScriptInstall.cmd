c5801a8bf42341f221acde02ce2e8a2309f117b2
raje678 <40144119+raje678@users.noreply.github.com>
2018-06-22 13:21:48 -0500

    Update DBScriptInstall.cmd

--- deployment/postdeployactions/DBScriptInstall.cmd
+++ deployment/postdeployactions/DBScriptInstall.cmd
@@ -1,18 +1,18 @@
 
-SET SERVER=%APPSETTING_key1%
-SET DB="pwadb"
-SET LOGIN=%APPSETTING_key2%
-SET PASSWORD=%APPSETTING_key3%
-SET INPUT1="D:\home\site\wwwroot\scripts\Database_Schema.sql"
-SET OUTPUT1="D:\home\site\wwwroot\scripts\DB_Schema_OutputLog.txt"
+SET "SERVER"=%APPSETTING_key1%
+SET "DB"="pwadb"
+SET "LOGIN"=%APPSETTING_key2%
+SET "PASSWORD=%APPSETTING_key3%
+SET "INPUT1"=D:\home\site\wwwroot\scripts\Database_Schema.sql
+SET "OUTPUT1"=D:\home\site\wwwroot\scripts\DB_Schema_OutputLog.txt
 SQLCMD -S %SERVER% -d %DB% -U %LOGIN% -P %PASSWORD% -i %INPUT1% -o %OUTPUT1% -b
 IF %ERRORLEVEL% == 1 (
 ECHO THERE WAS AN ERROR - to file >> %OUTPUT%
 ECHO THERE WAS AN ERROR - on screen)
 
 
-SET INPUT2="D:\home\site\wwwroot\scripts\config_data.sql"
-SET OUTPUT2="D:\home\site\wwwroot\scripts\Config_Data_OutputLog.txt"
+SET "INPUT2"=D:\home\site\wwwroot\scripts\config_data.sql
+SET "OUTPUT"=D:\home\site\wwwroot\scripts\Config_Data_OutputLog.txt
 SQLCMD -S %SERVER% -d %DB% -U %LOGIN% -P %PASSWORD% -i %INPUT2% -o %OUTPUT2% -b
 IF %ERRORLEVEL% == 1 (
 ECHO THERE WAS AN ERROR - to file >> %OUTPUT%
