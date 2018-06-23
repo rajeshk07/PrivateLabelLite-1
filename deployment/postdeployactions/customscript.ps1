d4b07baa9ae09ecd24e4f915cb46078729a76662
040305 <040305@USHOM00NB040305>
2018-06-22 19:46:07 -0500

    1.30

--- deployment/postdeployactions/customscript.ps1
+++ deployment/postdeployactions/customscript.ps1
@@ -8,6 +8,15 @@ $psdsql = $env:APPSETTING_key3;
 $webConfig = 'D:\HOME\site\wwwroot\Web.config'
 $doc = (Get-Content $webConfig) -as [Xml]
 
+$obj = $doc.configuration.appSettings.add | where {$_.Key -eq 'MicrosoftClientId'}
+$obj.value = $env:APPSETTING_key5;
+
+$objpas = $doc.configuration.appSettings.add | where {$_.Key -eq 'MicrosoftClientSecret'}
+$objpas.value = $env:APPSETTING_key6;
+
+$allowres = $doc.configuration.appSettings.add | where {$_.Key -eq 'AllowedResellers'}
+$allowres.value = $env:APPSETTING_key7;
+
 $root = $doc.get_DocumentElement();
 $pllConString = $root.connectionStrings.add | ? {$_.name -eq 'PLLConnection'}
 $pllnewCon = $pllConString.connectionString.Replace('Server=;Initial Catalog=;Persist Security Info=False;User ID=;Password=;','Server='+$srvsql+';Initial Catalog=pwadb;Persist Security Info=False;User ID='+$usrsql+';Password='+$psdsql+';');
