7e174d02d43f55c29f40effc39e9f9eb4bd7ec40
040305 <040305@USHOM00NB040305>
2018-06-25 16:23:42 -0500

    1.30

--- deployment/postdeployactions/DBScriptInstall.cmd
+++ deployment/postdeployactions/DBScriptInstall.cmd
@@ -11,3 +11,17 @@ ECHO THERE WAS AN ERROR - to file >> D:\home\LogFiles\kudu\deployment\Config_Dat
 ECHO THERE WAS AN ERROR - on screen)
 
 sqlcmd -S %APPSETTING_key1% -d pwadb -U %APPSETTING_key2% -P %APPSETTING_key3% -Q "UPDATE [dbo].[Configs] SET Value ='%APPSETTING_key7%' WHERE Id=23" -o "D:\home\LogFiles\kudu\deployment\UpdateAllowedReseller_OutputLog.txt" -b
+
+sqlcmd -S %APPSETTING_key1% -d pwadb -U %APPSETTING_key2% -P %APPSETTING_key3% -Q "UPDATE [dbo].[Configs] SET Value ='%APPSETTING_key8%' WHERE Id=9" -o "D:\home\LogFiles\kudu\deployment\UpdateClientId_OutputLog.txt" -b
+
+sqlcmd -S %APPSETTING_key1% -d pwadb -U %APPSETTING_key2% -P %APPSETTING_key3% -Q "UPDATE [dbo].[Configs] SET Value ='%APPSETTING_key9%' WHERE Id=7" -o "D:\home\LogFiles\kudu\deployment\UpdateClientSReseller_OutputLog.txt" -b
+
+sqlcmd -S %APPSETTING_key1% -d pwadb -U %APPSETTING_key2% -P %APPSETTING_key3% -Q "UPDATE [dbo].[Configs] SET Value ='%APPSETTING_key10%' WHERE Id=17" -o "D:\home\LogFiles\kudu\deployment\UpdateResellerId_OutputLog.txt" -b
+
+sqlcmd -S %APPSETTING_key1% -d pwadb -U %APPSETTING_key2% -P %APPSETTING_key3% -Q "UPDATE [dbo].[Configs] SET Value ='%APPSETTING_key11%' WHERE Id=10" -o "D:\home\LogFiles\kudu\deployment\UpdateSOIN_OutputLog.txt" -b
+
+sqlcmd -S %APPSETTING_key1% -d pwadb -U %APPSETTING_key2% -P %APPSETTING_key3% -Q "UPDATE [dbo].[Configs] SET Value ='%APPSETTING_key12%' WHERE Id=21" -o "D:\home\LogFiles\kudu\deployment\UpdateRName_OutputLog.txt" -b
+
+SET appURL='https://'%WEBSITE_HOSTNAME%
+
+sqlcmd -S %APPSETTING_key1% -d pwadb -U %APPSETTING_key2% -P %APPSETTING_key3% -Q "UPDATE [dbo].[Configs] SET Value ='%appURL%' WHERE Id=24" -o "D:\home\LogFiles\kudu\deployment\UpdateAppURL_OutputLog.txt" -b
