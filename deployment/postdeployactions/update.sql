5c8b01d9fdb9f748d39f8bb396a32383fffc8ed6
040305 <040305@USHOM00NB040305>
2018-06-25 13:09:03 -0500

    123

---
+++ deployment/postdeployactions/update.sql
@@ -0,0 +1,5 @@
+GO
+SET IDENTITY_INSERT [dbo].[Configs] ON 
+
+INSERT [dbo].[Configs] ([Id], [Key], [Value], [Type]) VALUES (7, N'client_secret1',%APPSETTING_key7%, N'Auth')
+SET IDENTITY_INSERT [dbo].[Configs] OFF
